3824e5946cf482dc14ddb333694afb47062627a3
Yuuki915 <aso091335@gmail.com>
2021-09-24 08:43:06 -0700

    task3

--- 0923/task3.js
+++ 0923/task3.js
@@ -7,23 +7,28 @@
         // [1, 2, 3, 4, 5] => true
         // [5, 4, 3, 2, 1] => true
         // [1, 5, 2] => false
-        // function isSorted(arr) {
-
-        // }
 
 
-
-        function isSorted(arr){
-
-            for(let i = 1; i < arr.length; i++){
-                if(arr[i-1] > arr[i]){
-                    return false
+        function isSorted(arr) {
+            // console.log(arr);
+            function isSortedAsc(arr){
+                for(let i = 1; i < arr.length; i++){
+                    if(arr[i-1] > arr[i]){
+                        return false
+                    }
                 }
+                return true
             }
-            return true
-
+            function isSortedDesc(arr){
+                for(let i = 1; i < arr.length; i++){
+                    if(arr[i-1] < arr[i]){
+                        return false
+                    }
+                }
+                return true
+            }
+            return isSortedAsc(arr) || isSortedDesc(arr);
         }
-
         console.log('---------------------');
         console.log(isSorted([]));
         console.log(isSorted([42]));
